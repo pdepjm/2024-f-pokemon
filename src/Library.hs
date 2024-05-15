fa2d422e749b30c082aa4e4bf4a05b70351e2a4a
Diana López Alvas <48812037+dlopezalvas@users.noreply.github.com>
2024-05-15 20:45:13 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -3,13 +3,13 @@ import PdePreludat
 
 data TipoPokemon = Planta | Agua | Fuego deriving (Show, Eq)
 
-tieneVentajaContra :: TipoPokemon -> TipoPokemon -> Boolean
+tieneVentajaContra :: TipoPokemon -> TipoPokemon -> Bool
 tieneVentajaContra Planta Agua = True
 tieneVentajaContra Agua Fuego = True
 tieneVentajaContra Fuego Planta = True
 tieneVentajaContra _ _ = False
 
 data Gimnasio = UnGimnasio {
-    nombre:: String,
-    siguiente:: Destino
+    nombre :: String,
+    siguiente :: Destino
 }
